850cd59d23242143efca1c32722c3f89b173aa71
Matthew Weidman <weidman.matthew@gmail.com>
2017-11-15 19:51:47 -0500

    color_mapping functions

--- color.mli
+++ color.mli
@@ -18,3 +18,14 @@ val make_color : int -> color
  * tuples [(l1, l2, c)], in which [c] is the color used on the text
  * from locations [l1] to [l2]. *)
 val cm_to_list : color_mapping -> (location * location * color) list
+
+(* [make_cm l] takes a list of [(l1, l2, c)] tuples where [c] is the
+ * color from [l1] to [l2] and produces a new color mapping. *)
+val make_cm : (location * location * color) list -> color_mapping
+
+(* [add_color cm (l1, l2, c)] adds a new color [c] from [l1] to [l2]. *)
+val add_color : color_mapping -> (location * location * color) -> color_mapping
+
+(* [empty_cm] is the default color mapping in which all text is the
+ * default color. *)
+val empty_cm : color_mapping
